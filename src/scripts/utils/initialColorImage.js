27a5bba0c30f090b46e86ab56cf636b78281f93d
syibran malawi <branmalawi@gmail.com>
2023-01-29 11:27:26 +0700

    update detail page v-1

--- src/scripts/utils/initialColorImage.js
+++ src/scripts/utils/initialColorImage.js
@@ -4,36 +4,21 @@
 /* eslint-disable no-param-reassign */
 import { prominent, average } from 'color.js';
 
-const img = new Image();
-const imgUrl = 'https://image.tmdb.org/t/p/w500/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg';
-const ggUrl = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=';
-img.src = ggUrl + encodeURIComponent(imgUrl);
-img.crossOrigin = 'Anonymous';
-const color = async () => {
-  const data = await prominent(img, { amount: 3 });
-  return data;
-};
-
 const InitialColorImage = {
-  async init({ main }) {
-    main.innerHTML = '';
-    main.innerHTML += `
-      <img src="${imgUrl}" alt="" class="w-[250px] rounded-md"/>`;
-    let colorData = await color();
-    colorData = colorData[2];
-    // colorData.forEach((data) => {
-    //   main.innerHTML += `
-    //       <div class="w-16 h-16 rounded-md inline-block" style="background-color: rgb(${data[0]},${data[1]},${data[2]});"></div>
-    //     `;
-    // });
-    main.innerHTML += `
-        <div class="w-16 h-16 rounded-md inline-block" style="background-color: rgb(${colorData[0]},${colorData[1]},${colorData[2]});"></div>
-      `;
-
-    const contrast = this.checkContrast(colorData);
-    main.innerHTML += `
-        <div class="w-16 h-16 rounded-md inline-block" style="background-color: ${contrast};"></div>
-      `;
+  async init(srcImage) {
+    const img = new Image();
+    const gglUrl = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=';
+    img.src = gglUrl + encodeURIComponent(srcImage);
+    img.crossOrigin = 'Anonymous';
+    const color = async () => {
+      const data = await prominent(img, { amount: 3 });
+      return data;
+    };
+    const colorData = await color();
+    return {
+      bgColor: colorData[2],
+      textColor: this.checkContrast(colorData[2]),
+    };
   },
 
   checkContrast(data) {
--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,43 +1,44 @@
 import NontonDatabase from '../../data/nonton-database';
 import UrlParser from '../../routes/url-parser';
+import InitialColorImage from '../../utils/initialColorImage';
 
 const Detail = {
   async render() {
     return `
-    <div class="bg-no-repeat bg-top bg-[length:600px] md:bg-cover pt-52 pb-1 sm:pb-0" style="background-image : url('./img/backdrop-11.webp'); background-color: rgba(20,40,80,1);">
-      <div class="w-full backdrop-blur-xl rounded-t-3xl h-fit detail__container py-[6px] gap-3" style="background-color: transparent; color: white;">
-        <div class="detail__image mt-[-60%] rounded-2xl overflow-hidden w-full aspect-[2/3]">
+    <div id="detail" class="bg-no-repeat bg-top bg-[length:600px] sm:bg-cover pt-52 sm:pt-0 pb-1 sm:pb-0">
+      <div class="w-full backdrop-blur-xl sm:backdrop-blur-0 rounded-t-3xl sm:rounded-t-none h-fit detail__container py-[6px] sm:p-5 md:p-7 gap-3 md:gap-8">
+        <div class="detail__image mt-[-60%] sm:mt-0 rounded-2xl overflow-hidden w-full aspect-[2/3]">
           <img src="./img/poster-11.webp" class=""/>
         </div>
-        <div class="h-full w-full detail__name">
-          <h2 class="font-semibold m-0 p-0">Thor Ragnarok</h2>
-          <p class="text-xs font-light text-white/80 mt-1"><span>24 Oct, 2017</span><span> &bull; </span><span>2h 11m</span></p>
+        <div class="h-fit w-full detail__name">
+          <h2 class="sm:text-[38] sm:leading-8 md:text-[40px] md:leading-[50px] font-semibold md:font-bold">Thor Ragnarok</h2>
+          <p class="text-xs font-normal md:text-base"><span>24 Oct, 2017</span><span> &bull; </span><span>2h 11m</span></p>
           <div class="flex flex-wrap w-full gap-[5px] mt-2">
-            <a href="#" class="px-3 py-0.5 rounded-full block text-xs font-normal border-[1.3px] whitespace-nowrap">Action</a>
-            <a href="#" class="px-3 py-0.5 rounded-full block text-xs font-normal border-[1.3px] whitespace-nowrap">Adventure</a>
-            <a href="#" class="px-3 py-0.5 rounded-full block text-xs font-normal border-[1.3px] whitespace-nowrap">Science Fiction</a>
-            <a href="#" class="px-3 py-0.5 rounded-full block text-xs font-normal border-[1.3px] whitespace-nowrap">Comedy</a>
+            <a href="#" class="px-3 py-0.5 rounded-full block text-xs md:text-sm font-normal border-[1.5px] whitespace-nowrap">Action</a>
+            <a href="#" class="px-3 py-0.5 rounded-full block text-xs md:text-sm font-normal border-[1.5px] whitespace-nowrap">Adventure</a>
+            <a href="#" class="px-3 py-0.5 rounded-full block text-xs md:text-sm font-normal border-[1.5px] whitespace-nowrap">Science Fiction</a>
+            <a href="#" class="px-3 py-0.5 rounded-full block text-xs font-normal border-[1.5px] whitespace-nowrap">Comedy</a>
           </div>
         </div>
-        <div class="h-full w-full detail__ovr">
-          <div class="flex justify-around mt-3">
+        <div class="h-fit w-full detail__ovr">
+          <div class="flex justify-around sm:justify-between mt-3 w-full sm:max-w-sm">
             <rate-bar value="76" class="relative rate-progress w-[50px] flex justify-center items-center text-sm bg-gray-800/50 backdrop-blur before:bg-gray-800  text-sky-50"></rate-bar>
-            <div class="w-[50px] h-[50px] bg-slate-500 flex justify-center items-center rounded-full">
-              <object data="./icon/watchlist.svg" type="image/svg+xml" class="sm:hidden w-6"></object>
+            <div class="w-[50px] h-[50px] bg-slate-400/70 backdrop-blur-md flex justify-center items-center rounded-full">
+              <object data="./icon/watchlist.svg" type="image/svg+xml" class="w-6"></object>
             </div>
-            <div class="w-[50px] h-[50px] bg-slate-500 flex justify-center items-center rounded-full">
-              <object data="./icon/favorite.svg" type="image/svg+xml" class="sm:hidden w-6"></object>
+            <div class="w-[50px] h-[50px] bg-slate-400/70 backdrop-blur-md flex justify-center items-center rounded-full">
+              <object data="./icon/favorite.svg" type="image/svg+xml" class="w-6"></object>
             </div>
-            <div class="w-[50px] h-[50px] bg-slate-500 flex justify-center items-center rounded-full">
-              <object data="./icon/play.svg" type="image/svg+xml" class="sm:hidden w-6"></object>
+            <div class="w-[50px] h-[50px] bg-slate-400/70 backdrop-blur-md flex justify-center items-center rounded-full">
+              <object data="./icon/play.svg" type="image/svg+xml" class="w-6"></object>
             </div>
           </div>
           <section class="py-3 w-full my-4">
-            <i class="font-light text-center w-full inline-block text-base">“No Hammer. No Problem.”</i>
+            <i class="font-light text-center sm:text-start sm:font-normal w-full inline-block text-base">“No Hammer. No Problem.”</i>
           </section>
-          <section id="overview" class="px-3 mb-5">
+          <section id="overview" class="px-3 sm:px-0 md:pr-20 mb-5">
             <h1 class="text-xl font-semibold px-2">Overview</h1>
-            <p class="text-base font-normal text-white/70">Thor is imprisoned on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the destruction of his home-world and the end of Asgardian civilization, at the hands of a powerful new threat, the ruthless Hela.</p>
+            <p class="text-base font-normal">Thor is imprisoned on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the destruction of his home-world and the end of Asgardian civilization, at the hands of a powerful new threat, the ruthless Hela.</p>
           </section>
         </div>
       </div>
@@ -45,16 +46,17 @@ const Detail = {
     <section id="cast" class="px-2 mt-3">
     <h1 class="text-xl text-bold px-3">Cast</h1>
     <div class="p-2 pb-6 flex gap-[2%] min-[700px]:gap[1.5%] min-[890px]:gap-[1%] overflow-y-hidden custom-scrollbar">
-      <image-item-skeleton class="image-item animate-pulse"></image-item-skeleton>
-      <image-item-skeleton class="image-item animate-pulse"></image-item-skeleton>
-      <image-item-skeleton class="image-item animate-pulse"></image-item-skeleton>
-      <image-item-skeleton class="image-item animate-pulse"></image-item-skeleton>
-      <image-item-skeleton class="image-item animate-pulse"></image-item-skeleton>
-      <image-item-skeleton class="image-item animate-pulse"></image-item-skeleton>
-      <image-item-skeleton class="image-item animate-pulse"></image-item-skeleton>
-      <image-item-skeleton class="image-item animate-pulse"></image-item-skeleton>
+      <image-item-skeleton class="person-item animate-pulse"></image-item-skeleton>
+      <image-item-skeleton class="person-item animate-pulse"></image-item-skeleton>
+      <image-item-skeleton class="person-item animate-pulse"></image-item-skeleton>
+      <image-item-skeleton class="person-item animate-pulse"></image-item-skeleton>
+      <image-item-skeleton class="person-item animate-pulse"></image-item-skeleton>
+      <image-item-skeleton class="person-item animate-pulse"></image-item-skeleton>
+      <image-item-skeleton class="person-item animate-pulse"></image-item-skeleton>
+      <image-item-skeleton class="person-item animate-pulse"></image-item-skeleton>
     </div>
-  </section>     
+  </section>    
+  
    `;
   },
 
@@ -62,6 +64,45 @@ const Detail = {
     const detailSrc = UrlParser.parseActiveUrlWithoutCombiner();
     const detailData = await NontonDatabase.detail(detailSrc.verb, detailSrc.id);
     console.log(detailData);
+    const detail = document.querySelector('#mainContent');
+
+    const dataColor = await InitialColorImage.init(`https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${detailData.poster_path}`);
+    console.log(dataColor);
+    const warna = dataColor.bgColor.join(',');
+
+    const image = document.querySelector('.detail__image > img');
+    image.src = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${detailData.poster_path}`;
+
+    detail.innerHTML += `<style>
+    #detail {
+      background-image : url('https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/${detailData.backdrop_path}');
+      background-color: rgba(${warna},1); 
+    }
+
+    #detail .detail__name {
+      color: white;
+      border-color: blue;
+    }
+
+    #detail .detail__ovr {
+      color: ${dataColor.textColor};
+    }
+    
+    @media screen and (min-width: 640px) {
+      #detail>div {
+        background-color: rgba(${warna},.8); 
+      }
+
+      #detail .detail__name {
+        color: ${dataColor.textColor};
+      }
+
+      #detail .detail__name a {
+        border-color: ${dataColor.textColor};
+      }
+      
+    }
+  </style> `;
   },
 };
 
